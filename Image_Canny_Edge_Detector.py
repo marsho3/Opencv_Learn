6de6595a5a8f6ccfb2ff1f5ea54ea84c1c0e34b1
marsho3 <56955290+marsho3@users.noreply.github.com>
2020-03-08 17:50:17 +0800

    Updated Path of Canny Edge Detector

--- Image_Canny_Edge_Detector.py
+++ Image_Canny_Edge_Detector.py
@@ -32,27 +32,27 @@ def Tunning_pheripherals():
     cv2.setTrackbarPos('Ratio',window_name,1)
 
 
-# In[20]:
+# In[5]:
 
 
 def main():
-    raw_image = cv2.imread("Macintosh HD/Users\kaisiuho\ImageProcess\lena512rgb.png")
+    raw_image = cv2.imread("/Users/kaisiuho/ImageProcess/lena512rgb.png")
     cv2.imshow('Raw',raw_image)
-    Tunning_peripherals()
+    Tunning_pheripherals()
     while True:
-        thresold = cv2.getTrackbarPos('Threshold',window_name)
+        threshold = cv2.getTrackbarPos('Threshold',window_name)
         ratio = cv2.getTrackbarPos('Ratio',window_name)
         
-        gray_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+        gray_image = cv2.cvtColor(raw_image,cv2.COLOR_BGR2GRAY)
         #Smooth image with GassianBlur where kernel size is 5x5
         smoothed = cv2.GaussianBlur(gray_image,(5,5),0)
         edges = cv2.Canny(smoothed,threshold,threshold*ratio)
-        print("Canny Threshold:", threshold,"and",threshold*ratio)
+        print("Canny Threshold:",threshold,"and",threshold*ratio)
         cv2.imshow("Edge Detection",edges)
         cv2.waitKey(1)
 
 
-# In[21]:
+# In[ ]:
 
 
 if __name__ == '__main__':
